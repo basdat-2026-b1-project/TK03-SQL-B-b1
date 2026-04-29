9b0eca654a3f682f892ad8ce483b503e36e575d4
Cathlin Abigail <cathlin.abigail@ui.ac.id>
2026-04-30 00:36:38 +0700

    sebelum navbar footer

--- aero_miles/settings.py
+++ aero_miles/settings.py
@@ -57,10 +57,11 @@ ROOT_URLCONF = 'aero_miles.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -68,7 +69,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'aero_miles.wsgi.application'
 
 
